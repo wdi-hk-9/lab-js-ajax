7bef06a50e643e5ff508c3d73e35cafe5c601e64
Jacky Lee <jackywhlee@gmail.com>
2015-11-17 03:21:57 +0800

    just got post to work... oh boy

--- js/jquery.app.js
+++ js/jquery.app.js
@@ -1,1 +1,33 @@
 // Your solution here
+$().ready(function(){
+
+  $.get('https://api.doughnuts.ga/doughnuts').done(function(data){
+    data.forEach(function (elem){
+      $('#doughnuts').append('<li>Flavor:' + elem.flavor + ' | Style:' + elem.style + '</li>')
+    })
+
+  });
+
+
+  $('#new-doughnut').on('submit', function(e){
+    e.preventDefault();
+    console.log('form clicked')
+    $.ajax('https://api.doughnuts.ga/doughnuts',
+      {
+        method:'POST',
+        data: {
+          flavor:$('#new-doughnut-flavor').val(),
+          style: $('#new-doughnut-style').val()
+        }
+      })
+      .done(function(data){
+        $('#doughnuts').append(
+          '<li> ' +
+            data.flavor +
+            ' | ' + data.style +
+          '</li>'
+        );
+    })
+
+  })
+})
