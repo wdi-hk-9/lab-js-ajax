334da2297642bde0988b873d09f545a512337b15
Charlotte Kwan <kwan.charlotte.cl@gmail.com>
2015-11-17 00:33:04 +0800

    modal works

--- js/jquery.app.js
+++ js/jquery.app.js
@@ -3,7 +3,14 @@ $().ready(function(){
   // console.log('dom loaded')
   var addDonut = function(donut){
     $('#doughnuts').append('<li data-id="'+donut.id+'" class="h5">'+ donut.flavor +'-' + donut.style +'<button type="button" class="edit btn btn-success btn-group-xs">Edit</button>'+'<button type="button" class="btn btn-danger btn-group-xs">Delete</button></li>');
-  };
+  var editBtn = $(".edit").on('click',function(e){
+    e.stopPropagation();
+    var id = $(this).parent().data('id');
+    $('#edit-doughnut').data('id', id);
+    $('#edit-modal').modal('show');
+  });
+};
+
 
   $.get('https://api.doughnuts.ga/doughnuts')
     .done(function(doughnuts){
@@ -27,32 +34,27 @@ $().ready(function(){
     })
   })
 
-  $(".edit").on('click',function(e){
-    e.stopPropagation();
+  var editBtn = $(".edit").on('click',function(e){
     var id = $(this).parent().data('id');
     $('#edit-doughnut').data('id', id);
     $('#edit-modal').modal('show');
+    $('#edit-doughnut').on('submit', function(){
+      e.preventDefault();
+      var getid = $('#edit-doughnut').data('id');
+      var donutUrl = 'https://api.doughnuts.ga/doughnuts/' + getid;
+      $.ajax({
+        url: donutUrl,
+        type: 'PUT',
+        data: {
+          flavor: $('#edit-doughnut-flavor').val(),
+          style: $('#edit-doughnut-style').val()
+        }})
+      .done(function(donut){
+        console.log(donut);
+      })
+    })
   });
 
-// .ajax('https://api.doughnuts.ga/doughnuts/2',{ method: 'PUT', data: {flavor: 'Banana'}}).done(function(data){console.log(data);});
-  $('#edit-doughnut').on('submit', function(){
-    e.preventDefault();;
-    var getid = $('#edit-doughnut').data('id');
-    var donutUrl = 'https://api.doughnuts.ga/doughnuts/' + getid;
-    $.ajax({
-      url: donutUrl,
-      type: 'PUT',
-      data: {
-        flavor: $('#edit-doughnut-flavor').val(),
-        style: $('#edit-doughnut-style').val()
-      }})
-    .done(function(donut){
-      console.log(donut);
-    })
-  })
-
 // $.ajax('https://api.doughnuts.ga/doughnuts/10',{ method: 'DELETE'}).done(function(data){console.log(data);});
-
-
 
 })
